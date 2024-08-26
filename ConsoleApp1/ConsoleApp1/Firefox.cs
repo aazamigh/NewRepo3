b6a3ec7284a79b1d9ff5d323369718f7329dd53d
ali <ali@a.a>
2024-08-26 06:58:52 +0330

    Add project files.

---
+++ ConsoleApp1/ConsoleApp1/Firefox.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Firefox
+    {
+        public List<string> Names()
+        {
+            var list=new List<string>();
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add("Firefox"+i.ToString());
+            }
+            return list;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+
+
+Firefox firefox = new Firefox();
+foreach (var item in firefox.Names())
+{
+    Console.WriteLine(item);
+}
